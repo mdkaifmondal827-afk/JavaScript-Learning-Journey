1861fb6f2f5115246dfe2c05f29c7a19a467c140
mdkaifmondal827-afk <mdkaifmondal827@gmail.com>
2026-06-14 14:21:51 +0000

    Added new files

---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 14453;
+let accountEmail = "kaifmondal@gmail.com"
+var accountPassword = "12345"
+  accountCity = "Kolkata"
+  let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hccc@gmail.com"
+accountPassword = "2134"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+/*
+Prefernot to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
